790b6024789f91b6215243f7966103b40d6305c4
Shaun McCormick <68571888+sm624@users.noreply.github.com>
2022-02-09 09:44:50 -0500

    Update db.sql
    forgot to add primary key commands woops

--- db.sql
+++ db.sql
@@ -11,12 +11,15 @@ CREATE TABLE Game(
   UpVotes int,
   Description varchar(255),
   Year int,
-  Download varchar(255)
+  Download varchar(255),
+  PRIMARY KEY(GameID)
 );
 CREATE TABLE Creator(
   CreatorID int,
   Name varchar(255),
   Games varchar(255),
   Description varchar(255),
-  Github varchar(255)
+  Github varchar(255),
+  PRIMARY KEY (CreatorID)
 );
+/*this could have a 4th table but the project needs to be further along to determine this
