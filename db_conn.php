d978f44783960365f4ca5bd29d3a8ebba6893aa2
Shaun McCormick <68571888+sm624@users.noreply.github.com>
2022-02-14 16:57:24 -0500

    Add files via upload
    this is the database connection file you use to connect the front end to the database, i can take care of it but if anyone wanted to know how it works ill explain

---
+++ db_conn.php
@@ -0,0 +1,8 @@
+<?php
+    $db_location = "localhost";
+    $username = "GameRepo";
+    $password = "csWebPass!";
+    $db_name = "GameRepo";
+    
+    $conn = mysqli_connect($db_location, $username, $password, $db_name);
+?>
