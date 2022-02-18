0e16a0f676e0ba1735df530f2c87fc3feee0dc70
Shaun McCormick <68571888+sm624@users.noreply.github.com>
2022-02-18 16:16:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  	<head>
+		<title>Game Repo</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	
+	<body>
+		<div class="container-fluid px-md-5">
+			<div class="row justify-content-between">
+				<div class="col-md-8 order-md-last">
+					<div class="row">
+						<div class="col-md-6 text-center">
+							<a class="navbar-brand" href="index.html"> <img id="logo-main" src="Edinboro_University_logo.png">
+								<span>Game Repository</span></a>
+						</div>
+						<div class="col-md-6 d-md-flex justify-content-end mb-md-0 mb-3">
+							<form action="#" class="searchform order-lg-last">
+			          <div class="form-group d-flex">
+			            <input type="text" class="form-control pl-3" placeholder="Search">
+			            <button type="submit" placeholder="" class="form-control search"><span class="fa fa-search"></span></button>
+			          </div>
+			        </form>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 d-flex">
+					<div class="social-media">
+		    		<p class="mb-0 d-flex">
+		    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+		    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+		    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+		    		</p>
+	        </div>
+				</div>
+			</div>
+		</div>
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container-fluid">
+	    
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav m-auto">
+	        	<li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+	        	<li class="nav-item dropdown">
+            </li>
+	        	<li class="nav-item"><a href="#" class="nav-link">Games</a></li>
+	        	<li class="nav-item"><a href="#" class="nav-link">Creators</a></li>
+	          <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+
+	<div class="library">
+		<div>
+		<?php
+			include "db_conn.php";
+
+			$game = mysqli_query($conn, "SELECT * FROM GamePictures"); //this could also be pictures instead of games
+			$numRecords = mysqli_num_rows($game);
+
+			/*for($i=0;$i<$numRecords; $i++){
+				$row = mysqli_fetch_array($game);
+				echo "<tr>";
+				echo "<td>" . "<a href=" . $row["Path"] . "download=" . "'" . $row["GameID"] . "'" 
+				. "> <img src=" . $row["Path"] . " class='" . "game" . "'>"."</td>";
+			}*/
+
+		?>
+		</div>
+	</div>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
